97a11437ca87891d1ce6454cd7d96f8075fc9221
Zabir Arkam <zabirarkam27@gmail.com>
2025-04-18 03:16:04 +0600

    Added mongodb limit operator

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,63 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("productDB").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const limit = parseInt(req.query.limit) || 0;
+      const cursor = productCollection.find().limit(limit);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedEquipments = req.body;
+      const equipments = {
+        $set: {
+          name: updatedEquipments.name,
+          photo: updatedEquipments.photo,
+          category: updatedEquipments.category,
+          price: updatedEquipments.price,
+          stock: updatedEquipments.stock,
+          customization: updatedEquipments.customization,
+          description: updatedEquipments.description,
+          authorName: updatedEquipments.authorName,
+          email: updatedEquipments.email,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        equipments,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,18 +88,15 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('sport hive server is running')
-})
+app.get("/", (req, res) => {
+  res.send("sport hive server is running");
+});
 
 app.listen(port, () => {
-    console.log("Sport Hive server is running on port: " + port)
-})
+  console.log("Sport Hive server is running on port: " + port);
+});
